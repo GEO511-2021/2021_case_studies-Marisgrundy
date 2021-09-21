73af8642a3829bbaa4d34b65545c07037b4fc977
Marisgrundy <89953225+Marisgrundy@users.noreply.github.com>
2021-09-21 10:17:11 -0400

    first case study

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,16 @@
-data(iris)
+data("iris")
+library(ggplot2)
+petal_length_mean <- mean(iris$Petal.Length)
+
+Plot1<-ggplot(data=iris, aes(Petal.Length))+ 
+  geom_histogram(breaks=seq(0, 8, by=.5),
+                 col= ("red"),
+                 aes(fill=..count..)) +
+  scale_fill_gradient("Count", low="red", high="green")+
+  labs(title="Iris Petal Length", x="mean length", y="count")
+print(Plot1)
+
+png("First_Script.png")
+print(Plot1)
+dev.off()
 
